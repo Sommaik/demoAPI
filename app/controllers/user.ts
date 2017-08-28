9e737d8ead3100e88cccba5b2756994d4e47e343
sommaik <sommai.k@gmail.com>
2017-08-28 14:36:59 +0700

    Add user class

---
+++ app/controllers/user.ts
@@ -0,0 +1,17 @@
+import { Router, Request, Response } from 'express';
+import { MongoClient, ObjectID } from 'mongodb';
+import * as myConfig from 'config';
+import { mongodb } from '../helpers/mongodb';
+
+let config: any = myConfig.get('Config');
+
+const router: Router = Router();
+
+router.post('/', (req: Request, res: Response) => {
+    let data = req.body;
+    mongodb.collection("user").insertOne(data).then((data) => {
+        res.json(data);
+    });
+});
+
+export const UserController: Router = router;
