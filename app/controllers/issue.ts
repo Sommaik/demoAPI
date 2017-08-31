2a99facac87739a1149c72d47cdbc6eac79d308b
sommaik <sommai.k@gmail.com>
2017-08-31 13:32:48 +0700

    check exists folder

--- app/controllers/issue.ts
+++ app/controllers/issue.ts
@@ -1,19 +1,19 @@
-import {Router,Request,Response} from 'express';
-import {MongoClient,ObjectID} from 'mongodb';
+import { Router, Request, Response } from 'express';
+import { MongoClient, ObjectID } from 'mongodb';
 import { mongodb } from '../helpers/mongodb';
 import * as shortid from 'shortid';
 import * as multer from 'multer';
 import * as fs from 'fs';
 import * as myConfig from 'config';
 
-const router:Router=Router();
+const router: Router = Router();
 let config: any = myConfig.get('Config');
 
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        console.log(config.uploadPath+req.params.folderName);
-        let folder = config.uploadPath+req.params.folderName;
-        if(!fs.existsSync(folder)){
+        console.log(config.uploadPath + req.params.folderName);
+        let folder = config.uploadPath + req.params.folderName;
+        if (!fs.existsSync(folder)) {
             fs.mkdirSync(folder);
         }
         cb(null, folder);
@@ -25,115 +25,119 @@ var storage = multer.diskStorage({
 
 var upload = multer({ storage: storage });
 
-router.get('/',  (req:Request, res:Response) => {
-    mongodb.collection("issue").find().toArray().then((data)=> {
+router.get('/', (req: Request, res: Response) => {
+    mongodb.collection("issue").find().toArray().then((data) => {
         res.json(data);
     });
 });
 
 //get value by _id
-router.get('/findByID/:id',  (req:Request, res:Response) => {
-    let id=new ObjectID(req.params.id);
-    mongodb.collection("issue").findOne({_id:id})
-        .then((data)=> {
-        res.json(data);
-    });
+router.get('/findByID/:id', (req: Request, res: Response) => {
+    let id = new ObjectID(req.params.id);
+    mongodb.collection("issue").findOne({ _id: id })
+        .then((data) => {
+            res.json(data);
+        });
 });
 
 //get value by _id
-router.get('/:id',  (req:Request, res:Response) => {
-    let id=new ObjectID(req.params.id);
-    mongodb.collection("issue").findOne({_id:id})
-        .then((data)=> {
-        res.json(data);
-    });
+router.get('/:id', (req: Request, res: Response) => {
+    let id = new ObjectID(req.params.id);
+    mongodb.collection("issue").findOne({ _id: id })
+        .then((data) => {
+            res.json(data);
+        });
 });
 
-router.post('/',  (req:Request, res:Response) => {
+router.post('/', (req: Request, res: Response) => {
     //ข้อมูลที่ได้มากจากการ post จะเป็น req.body
     //insert into mongodb from post in postman
-    let data=req.body;
+    let data = req.body;
     data.no = shortid.generate();
-    mongodb.collection("issue").insert(data).then((data)=>{
+    mongodb.collection("issue").insert(data).then((data) => {
         res.json(data);
     });
     //res.json(req.body);
 });
 //search
-router.post('/search',  (req:Request, res:Response) => {
+router.post('/search', (req: Request, res: Response) => {
     //ข้อมูลที่ได้มากจากการ post จะเป็น req.body
     //insert into mongodb from post in postman
-    
-    let ret={
-        row:[],
-        total:Number
+
+    let ret = {
+        row: [],
+        total: Number
     };
-    let data=req.body;
+    let data = req.body;
     mongodb.collection("issue").find({
         title: new RegExp(`${data.searchText}`)
-    }).skip(data.numPage*data.rowPerPage)
-    .limit(data.rowPerPage)
-    .toArray().then((datas)=>{
-        ret.row=datas;
-        mongodb.collection("issue").find({
-        title: new RegExp(`${data.searchText}`)
-    }).count().then((num)=>{
-                ret.total=num; 
+    }).skip(data.numPage * data.rowPerPage)
+        .limit(data.rowPerPage)
+        .toArray().then((datas) => {
+            ret.row = datas;
+            mongodb.collection("issue").find({
+                title: new RegExp(`${data.searchText}`)
+            }).count().then((num) => {
+                ret.total = num;
                 res.json(ret);
             });
-       
-       
-    });
+
+
+        });
     //res.json(req.body);
 });
 //delete
 ///:id คือ parameter ที่รับเข้ามา ในรูปแบบของ url
-router.delete('/:id',  (req:Request, res:Response) => {
+router.delete('/:id', (req: Request, res: Response) => {
     // req.params.id คือ parameter ที่ได้ ObjectID คือ _id จาก mongodb
     // ถ้าเป็น ฟิล ธรรมดา ไม่ต้องใช้ ObjectID แค่เปลี่ยน _id เป็น ชื่อฟิล เลย
-    let id=new ObjectID(req.params.id);
-    mongodb.collection("issue").deleteOne({_id : id}).then((data)=>{
+    let id = new ObjectID(req.params.id);
+    mongodb.collection("issue").deleteOne({ _id: id }).then((data) => {
         res.json(data);
     });
 });
 
 //put
-router.put('/:id',  (req:Request, res:Response) => {
-   
-    let id=new ObjectID(req.params.id);
-    let data=req.body;
-    mongodb.collection("issue").updateOne({_id : id},data).then((data)=>{
+router.put('/:id', (req: Request, res: Response) => {
+
+    let id = new ObjectID(req.params.id);
+    let data = req.body;
+    mongodb.collection("issue").updateOne({ _id: id }, data).then((data) => {
         res.json(data);
     });
 });
 
-router.post('/attach/:folderName', 
+router.post('/attach/:folderName',
     upload.single('attach'),
-    (req:Request, res:Response)=>{
-    res.json({
-        success: true
+    (req: Request, res: Response) => {
+        res.json({
+            success: true
+        });
     });
-});
 
 router.get('/attach/:folderName',
-    (req:Request, res:Response)=>{
-    let folder = config.uploadPath+req.params.folderName; 
-    fs.readdir(folder, (err, files)=>{
-        res.json(files);
+    (req: Request, res: Response) => {
+        let folder = config.uploadPath + req.params.folderName;
+        if (fs.existsSync(folder)) {
+            fs.readdir(folder, (err, files) => {
+                res.json(files);
+            });
+        }else{
+            res.json([]);
+        }
     });
-});
 
 router.get('/view-attach/:folderName/:fileName', (req: Request, res: Response) => {
     fs.readFile(
-    `${config.uploadPath}${req.params.folderName}/${req.params.fileName}`
+        `${config.uploadPath}${req.params.folderName}/${req.params.fileName}`
         , (err, data) => {
-        if (!err) {
-            res.write(data);
-            res.end();
-        } else {
-            res.end();
-        }
-    });
+            if (!err) {
+                res.write(data);
+                res.end();
+            } else {
+                res.end();
+            }
+        });
 });
 
-export const IssueController:Router=router;
+export const IssueController: Router = router;
