253b542dde1d50a875d45c4d8f15583194a444b6
sommaik <sommai.k@gmail.com>
2017-09-01 10:12:52 +0700

    add client.connetc()

--- app/controllers/postgres/company.ts
+++ app/controllers/postgres/company.ts
@@ -5,7 +5,9 @@ const router: Router = Router();
 
 router.post('/', (req: Request, res: Response) => {
     let data = req.body;
-    const client = new pg.Client('postgres://isadmin:isadmin@localhost:5432/issue');
+    const client = new pg.Client(
+        'postgres://isadmin:isadmin@localhost:5432/issue'
+    );
     client.connect();
     const query = client.query(`
         insert into tb_company (comp_code, comp_name)
